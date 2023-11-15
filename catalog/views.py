f9446079b5370be184b6ea131f0f298eefdb036f
Veronika <veronika-mg@bk.ru>
2023-11-16 00:59:04 +0300

    исправление окна обратной связи

--- catalog/views.py
+++ catalog/views.py
@@ -10,5 +10,5 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
+        print(f'{name} {phone}: {message}')
     return render(request, 'catalog/contacts.html')
