6dd043130d961b398c390c872c820b9196658136
Veronika <veronika-mg@bk.ru>
2023-12-05 01:24:27 +0300

    Домашка 20.1 Работа с ORM в Django

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Категория')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='product/', **NULLABLE)
+    # category = models.CharField(max_length=100, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    date_of_creation = models.DateField(verbose_name='Дата создания', **NULLABLE)
+    last_modified_date = models.DateField(**NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}: {self.price}  ({self.category})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('category',)
