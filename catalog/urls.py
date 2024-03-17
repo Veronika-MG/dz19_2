c7a07a37eda1c6297519c2ea752ad9030cab0d06
Veronika <veronika-mg@bk.ru>
2024-03-18 00:07:52 +0300

    Домашка 20.2 Шаблонизация

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import home, contacts, index, categories, category_products
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', index, name='index'),
+    path('categories/', categories, name='categories'),
+    path('<int:pk>products/', category_products, name='category_products'),
+    path('contacts/', contacts, name='contacts')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render
 
+from catalog.models import Category, Product
+
 
 def home(request):
     return render(request, 'catalog/home.html')
 
+def index(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Магазин - Продукты'
+    }
+    return render(request, 'catalog/index.html', context)
+
+def categories(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Магазин - Категории'
+    }
+    return render(request, 'catalog/categories.html', context)
+
+def category_products(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title': f'Продукты категории {category_item.name}'
+    }
+    return render(request, 'catalog/products.html', context)
+
 
 def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'{name} {phone}: {message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
